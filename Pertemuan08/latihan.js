3ad891f2a9c7048a2613964c68fe0b0f006b9501
anissafebrian <anissafebrian51@gmail.com>
2024-11-24 10:31:59 +0700

    praktikum 8

---
+++ Pertemuan08/latihan.js
@@ -0,0 +1,12 @@
+
+    //deklarasi
+    let jari2 = 15
+    //inisialisasi variabel jari-jari
+    const PHI = 3.14
+    // menghitung keliling lingkaran dan menimpannya ke dalam variabel
+    let keliling = 2 * PHI * jari2
+     // menghitung luas lingkaran dan menimpannya ke dalam variabel
+    let luas = PHI * jari2
+    
+    document.write("keliling lingkaran dengan jari-jari " + jari2 + "=" + keliling);
+    document.write("<br/> sedangkan luasnya = " + luas )
